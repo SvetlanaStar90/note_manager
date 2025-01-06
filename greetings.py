927488a665ab5e8a8fe76172da6db899b889b218
SvetlanaStar90 <kutrukhina66@gmail.com>
2025-01-06 21:03:41 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Светлана"
+title = "Сдача работы"
+content = "Пройти все лекции, сдать тест"
+status = "активна"
+created_date = "05-01-2025"
+issue_date = "05-02-2025"
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
