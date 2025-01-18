04a2be3df5133d896a11300abcb4b5000a336dec
SvetlanaStar90 <kutrukhina66@gmail.com>
2025-01-18 22:19:44 +0400

    Update add_titles_loop.py

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -4,7 +4,7 @@ while title != ""  :
     print(title)
     title_name.append(title)
     title = input("Введите заголовок (или оставьте пустым для завершения) :")
-print("Заголовок заметки :")
+print("Заголовки заметки :")
 for title in title_name :
    print("-", title)
 
